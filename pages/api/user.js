d78e1b120e3e89f38b07d4be01a478538388f4f0
zhu-tom <zhutom01@gmail.com>
2021-03-22 16:09:39 -0400

    remove middleware

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,16 +1,11 @@
-import nextConnect from 'next-connect';
-import middleware from '../../middleware';
 import { withIronSession } from 'next-iron-session'
 
-const handler = nextConnect();
 
-handler.use(middleware);
-
-handler.get((req,res) => {
+const handler = (req,res) => {
   const { password, ...rest } = req.session.get("user");
 
   res.json(rest);
-});
+};
 
 export default withIronSession(handler, {
   password: "complex_password_at_least_32_characters_long",

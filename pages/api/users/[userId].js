bee273532667113e68d66fd2a7b53675f04c28e4
zhu-tom <zhutom01@gmail.com>
2021-03-22 18:12:08 -0400

    add async

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -1,7 +1,7 @@
 import User from "../../../models/user";
 import { connectToDatabase } from "../../../util/mongodb";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   await connectToDatabase();
   const { userId } = req.query;
   User.findById(userId, (err, doc) => {

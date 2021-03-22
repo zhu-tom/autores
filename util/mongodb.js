5c3eff7ef8c1a3e39c624df6016a4c340c2a30ba
zhu-tom <zhutom01@gmail.com>
2021-03-22 19:30:33 -0400

    mongo sucks

--- util/mongodb.js
+++ util/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectToDatabase() {
   if (!mongoose.connections[0].readyState) {
-    await mongoose.connect("mongodb+srv://"+ process.env.DB_USER + ":" + process.env.DB_PASS + "@" + process.env.DB_HOST + "/autores?retryWrites=true&w=majority", {
+    await mongoose.connect("mongodb+srv://admin:YNhkygw6K3G63yJS@cluster0.37ihr.mongodb.net/autores?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,

68919d386f2d4e9d12fe8b2ddc76e1612894108a
zhu-tom <zhutom01@gmail.com>
2021-03-23 00:06:11 +0000

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from crontab import CronTab
 
 cron = CronTab(True)
 
-command=f"cd {os.path.dirname(os.path.realpath(__file__))} && usr/local/bin/python3 {sys.argv[1]}.py {sys.argv[2]} {sys.argv[3]}"
+command=f"cd {os.path.dirname(os.path.realpath(__file__))} && /usr/bin/python3 {sys.argv[1]}.py {sys.argv[2]} {sys.argv[3]}"
 job = cron.new(command=command)
 job.minute.on(sys.argv[4])
 job.hour.on(sys.argv[5])
--- util/mongodb.js
+++ util/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectToDatabase() {
   if (!mongoose.connections[0].readyState) {
-    await mongoose.connect("mongodb+srv://admin:YNhkygw6K3G63yJS@cluster0.37ihr.mongodb.net/autores?retryWrites=true&w=majority", {
+    await mongoose.connect("mongodb://localhost:27017/autores", {
       useUnifiedTopology: true,
       useFindAndModify: false,
       useCreateIndex: true,

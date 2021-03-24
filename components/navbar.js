eb9ac7df8dd331c42dc5911ef6389b991f1b1adf
zhu-tom <zhutom01@gmail.com>
2021-03-24 18:04:21 -0400

    change navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,20 +1,16 @@
-import axios from 'axios';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
+import { useUser } from '../lib/hooks';
 
 export default function NavBar() {
-  const router = useRouter();
-  const logout = () => {
-    axios.delete("/api/auth")
-      .then(() => {
-        router.push("/login");
-      })
-  }
+  const {error} = useUser();
+
   return (
     <div className="w-screen flex p-4 bg-gray-300 justify-between">
       <Link href="/"><span className="cursor-pointer hover:text-blue-500">Home</span></Link>
-      <button className="hover:text-blue-500" onClick={() => logout()}>Logout</button>
+      <Link href={error ? "/login" : "/account"}>
+        <span className="hover:text-blue-500" onClick={() => handleClick()}>{error ? "Log In" : "Account"}</span>
+      </Link>
     </div>
   )
 }
--- pages/account.js
+++ pages/account.js
@@ -1,19 +1,30 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import Layout from "../components/layout";
 import { useUser } from "../lib/hooks";
 import { ACCOUNT_TYPE } from "../util/types";
 
 const PayPalButton = paypal.Buttons.driver("react", {React, ReactDOM});
 
 export default function Account() {
-  const { user } = useUser();
+  const { user, error} = useUser();
+  const router = useRouter();
 
   const [isPaid, setIsPaid] = useState(false);
   
   useEffect(() => {
+    if (error) router.push("/login");
     setIsPaid(user.type === ACCOUNT_TYPE.PAID);
   }, [user])
+
+  const logout = () => {
+    axios.delete("/api/auth")
+      .then(() => {
+        router.push("/login");
+      })
+  }
 
   const createOrder = (data, actions) => {
     return actions.order.create({
@@ -36,12 +47,17 @@ export default function Account() {
     });
   }
 
-  return isPaid ? (
-    <p>Already Paid</p>
-  ) : (
-    <PayPalButton
-      createOrder={createOrder}
-      onApprove={onApprove}
-    />
+  return (
+    <Layout>
+      <button onClick={() => logout()}>Log Out</button>
+      {isPaid ? (
+        <p>Already Paid</p>
+      ) : (
+        <PayPalButton
+          createOrder={createOrder}
+          onApprove={onApprove}
+        />
+      )}
+    </Layout>
   );
 }

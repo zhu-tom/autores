c55030fa7c1d584e508443b5c18875c05e3656b6
zhu-tom <zhutom01@gmail.com>
2021-03-24 17:55:50 -0400

    add if paid

--- pages/account.js
+++ pages/account.js
@@ -1,10 +1,20 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { useUser } from "../lib/hooks";
+import { ACCOUNT_TYPE } from "../util/types";
 
 const PayPalButton = paypal.Buttons.driver("react", {React, ReactDOM});
 
 export default function Account() {
+  const { user } = useUser();
+
+  const [isPaid, setIsPaid] = useState(false);
+  
+  useEffect(() => {
+    setIsPaid(user.type === ACCOUNT_TYPE.PAID);
+  }, [user])
+
   const createOrder = (data, actions) => {
     return actions.order.create({
       purchase_units: [
@@ -21,12 +31,14 @@ export default function Account() {
   const onApprove = (data, actions) => {
     return actions.order.capture().then(details => {
       axios.post("/api/payment").then(res => {
-        
+
       });
     });
   }
 
-  return (
+  return isPaid ? (
+    <p>Already Paid</p>
+  ) : (
     <PayPalButton
       createOrder={createOrder}
       onApprove={onApprove}
